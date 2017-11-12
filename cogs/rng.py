4abb5ed5425b1645cf0c946998c5d3788fa9ac4b
dash wav <dashwav@gmail.com>
2017-11-12 10:32:52 +0000

    rng.py created online with Bitbucket

---
+++ cogs/rng.py
@@ -0,0 +1,76 @@
+from discord.ext import commands
+import random as rng
+import discord
+
+class RNG():
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.answers = [
+            'For sure',
+            'Very doubtful',
+            'Most likely no',
+            'As I see it, yes',
+            'Probably not',
+            'Nope',
+            'Yes',
+            'Maybe',
+            'Most likely no',
+            'Ask again later',
+            'Definitely no',
+            'Not sure',
+            'It is uncertain',
+            'Definitely yes',
+            'Don\'t even think about it',
+            'Perhaps',
+            'NO - It may cause disease contraction',
+        ]
+        super().__init__()
+
+    @commands.group(pass_context=True)
+    async def random(self, ctx):
+        """Displays a random thing you request."""
+        if ctx.invoked_subcommand is None:
+            await ctx.send(f'Incorrect random subcommand passed. Try {ctx.prefix}help random')
+
+    @random.command()
+    async def number(self, ctx, minimum=0, maximum=100):
+        """Displays a random number within an optional range.
+        The minimum must be smaller than the maximum and the maximum number
+        accepted is 1000.
+        """
+
+        maximum = min(maximum, 1000)
+        if minimum >= maximum:
+            await ctx.send('Maximum is smaller than minimum.')
+            return
+
+        await ctx.send(rng.randint(minimum, maximum))
+
+    @commands.command()
+    async def choose(self, ctx, *choices: commands.clean_content):
+        """Chooses between multiple choices.
+        To denote multiple choices, you should use double quotes.
+        """
+        if len(choices) < 2:
+            return await ctx.send('Not enough choices to pick from.')
+
+        await ctx.send(rng.choice(choices))
+
+    @commands.command(name='8ball')
+    async def eightball(self, ctx, *, question):
+        """
+        Uses magic to determine the answer to a question
+        """
+        if not question:
+            return
+        answer = rng.choice(self.answers)
+        local_embed = discord.Embed(
+            title=f' ',
+            description=f'**Question**\n{question}\n:8ball: **8ball**{answer}',
+            color=0x419400
+        )
+        await ctx.send(local_embed)
+        
+def setup(bot):
+    bot.add_cog(RNG(bot))
